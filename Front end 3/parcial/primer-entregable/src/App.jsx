f4ce77bef2f189d65972af1245018387d8138519
olimarteam <73653954+olimarteam@users.noreply.github.com>
2023-08-15 23:14:56 -0300

    correccion entregable 1

--- Front end 3/parcial/primer-entregable/src/App.jsx
+++ Front end 3/parcial/primer-entregable/src/App.jsx
@@ -4,19 +4,21 @@ import './App.css'
 import Card from './Card'
 
 function App() {
-  
+
   const [data, setData] = useState('Hola, soy una tarjeta')
 
   return (
     <>
       <h2>primer entregable</h2>
-      <h1 className='h1'>Carga de estudiantes</h1>      
-      
+      <h1 className='h1'>Carga de estudiantes</h1>
+
       <form onSubmit={''}>
 
       </form>
-
-      <Card data={data}/>
+      
+      <div className='contenedor-cards'>
+        <Card data={data} />
+      </div>
     </>
   )
 }
