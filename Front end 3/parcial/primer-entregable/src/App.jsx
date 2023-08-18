e12db7202e8f883bc1085c6c226099a6ca1f9af3
olimarteam <73653954+olimarteam@users.noreply.github.com>
2023-08-17 22:54:44 -0300

    entregable1

--- Front end 3/parcial/primer-entregable/src/App.jsx
+++ Front end 3/parcial/primer-entregable/src/App.jsx
@@ -5,20 +5,66 @@ import Card from './Card'
 
 function App() {
 
-  const [data, setData] = useState('Hola, soy una tarjeta')
+  const [nombre, setNombre] = useState('')
+  const [equipo, setEquipo] = useState('')
+  const [error, setError] = useState(false)
+  const [verCard, setCards] = useState(false)
+  const [datosEnviados, setDatosEnviados] = useState({ nombre: '', equipo: '' });
+
+
+  const limpiaForm = () => {
+    setNombre('')
+    setEquipo('')
+  }
+
+  const handlerSubmit = (e) => {
+    e.preventDefault()
+    if (nombre.trim().length >= 3 && !nombre.startsWith(' ') && equipo.trim().length >= 6) {
+
+      setError(false)
+      setCards(true)
+      setDatosEnviados({ nombre, equipo })
+    }
+    else {
+      setError('Por favor chequea que la información sea correcta')
+      setCards(false)
+    }
+
+    limpiaForm()
+  }
+
+  const handleNombreChange = (e) => {
+    setError(false)
+    setNombre(e.target.value)
+  }
+  const handleEquipoChange = (e) => {
+    setError(false)
+    setEquipo(e.target.value)
+  }
 
   return (
     <>
       <h2>primer entregable</h2>
-      <h1 className='h1'>Carga de estudiantes</h1>
+      <h1 className='h1'>Carga de datos</h1>
 
-      <form onSubmit={''}>
+      <form className='form' onSubmit={handlerSubmit}>
+
+        <input type="text" placeholder='Ingrese tu nombre' value={nombre} onChange={handleNombreChange} />
+
+        <input type="text" placeholder='Ingresa tu equipo de futbol favorito' value={equipo} onChange={handleEquipoChange} />
+
+        <input type="submit" value="Enviar" />
 
       </form>
-      
-      <div className='contenedor-cards'>
-        <Card data={data} />
-      </div>
+
+      {error && <h2>{error}</h2>}
+
+      {verCard && !error && (
+        < div className='contenedor-cards'>
+          <Card nombre={datosEnviados.nombre} equipo={datosEnviados.equipo} />
+        </div >
+      )}
+
     </>
   )
 }
--- Front end 3/parcial/primer-entregable/src/Card.jsx
+++ Front end 3/parcial/primer-entregable/src/Card.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Card.css'
 
-const Card = ({data}) => {
+const Card = ({nombre, equipo}) => {
   return (
     <div className='card'>
-        {data}
+      
+      {nombre
+        ? <h2>Hola {nombre}</h2>
+        : <h2>Nombre</h2>
+      }
+
+      {equipo
+        ? <h3>Asi que sos hincha de {equipo}</h3>
+        : <h3>Equipo</h3>
+      }          
+
+      <h4>vas a comer del tapper!!</h4>
     </div>
   )
 }
