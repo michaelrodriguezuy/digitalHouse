0aba8734b0408e0277a6dd209aace11fd3bc9cdb
michaelrodriguezuy <hola@olimarteam.uy>
2024-05-09 09:19:42 -0300

    uso config-server siendo apuntado a mano, a traves del fichero bootstrap.yml en eureka-server

---
+++ especialidad backend/eureka/config1-server1/src/test/java/digitalHouse/config1server1/Config1Server1ApplicationTests.java
@@ -0,0 +1,13 @@
+package digitalHouse.config1server1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Config1Server1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
