a8920496c3abb968613f4d97593208f639739cbb
michaelrodriguezuy <hola@olimarteam.uy>
2024-10-03 01:45:36 -0300

    logs de algunos microservicios, y se levantan con kibana, elasticsearch y logstash

--- especialidad backend/intelliJ/payments/src/main/java/com/digitalhouse/payments/restcontroller/PaymentRestController.java
+++ especialidad backend/intelliJ/payments/src/main/java/com/digitalhouse/payments/restcontroller/PaymentRestController.java
@@ -2,10 +2,9 @@ package com.digitalhouse.payments.restcontroller;
 
 import com.digitalhouse.payments.model.Payment;
 import com.digitalhouse.payments.service.PaymentService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/payments")
@@ -13,8 +12,16 @@ public class PaymentRestController {
 
     private PaymentService paymentService;
 
+    Logger logger = LoggerFactory.getLogger(PaymentRestController.class);
+
     public PaymentRestController(PaymentService paymentService) {
         this.paymentService = paymentService;
+    }
+
+    @GetMapping()
+    public Payment getPayment(String id){
+        logger.info("Intentando obtener payment con id: "+id);
+        return new Payment(id,500.0f,"id 1", "id 2");
     }
 
     @PostMapping()
