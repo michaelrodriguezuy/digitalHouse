e3ca36d7732c01e4db8c828d1542b21c071c77f1
michaelrodriguezuy <hola@olimarteam.uy>
2025-03-24 15:50:00 -0300

    funciones avanzadas en GO

--- especialidad backend/go/functions/function/function.go
+++ especialidad backend/go/functions/function/function.go
@@ -1,7 +1,18 @@
 package function
 
 import (
+	"errors"
 	"fmt"
+)
+
+type Operation int //defino un tipo para ser usado como constante, que representa un tipo de operacion, ya sea suma, resta.. multiplicacion...
+
+const (
+	SUM Operation = iota
+	SUB
+	MUL
+	DIV
+	TEST
 )
 
 func Display(myValue int) {
@@ -9,14 +20,74 @@ func Display(myValue int) {
 	fmt.Printf("Type: %T, Value: %d, Address: %v\n", myValue, myValue, &myValue)
 }
 
-func Add (a, b int) int {
+func Add(a, b int) int {
 	return a + b
 }
 
-func RepeatString(increment int, value string)  {
-	
-	for i:= range  increment {
+func RepeatString(increment int, value string) {
+
+	for i := range increment {
 		fmt.Println(i, value)
 	}
-	
 }
+
+func Calc(op Operation, x, y float64) (float64, error) {
+	switch op {
+	case SUM:
+		return x + y, nil
+	case SUB:
+		return x - y, nil
+	case MUL:
+		return x * y, nil
+	case DIV:
+		if y != 0 {
+			return x / y, nil
+		}
+		return 0, errors.New("division by zero")
+	}
+
+	return 0, errors.New("invalid operation")
+
+}
+
+func Split(v int) (x, y int) {
+	x = v * 4 / 10 //multiplico el valor por 4 y luego divido por 10
+	y = v % 10     //obtengo el resto de la division por 10
+	return
+}
+
+func MSum(values ...float64) float64 {
+	var sum float64
+	for _, v := range values {
+		sum += v
+	}
+	return sum
+}
+
+func MOperation(op Operation, values ...float64) (float64, error) {
+	if len(values) == 0 { //si no hay valores, retorno 0 y un error
+		return 0, errors.New("no values")
+	}
+	sum := values[0] //el primer valor es el valor inicial
+
+	for _, v := range values[1:] { //recorro los valores desde el segundo al final
+
+		switch op {
+		case SUM:
+			 sum += v
+		case SUB:
+			 sum -= v
+		case MUL:
+			 sum *= v
+		case DIV:
+			if v != 0 {
+				sum /= v
+			} 
+			return 0, errors.New("division by zero")			
+		}
+	}
+	return sum, nil
+}
+
+
+
--- especialidad backend/go/functions/main.go
+++ especialidad backend/go/functions/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/michaelrodriguez/digitalhouse-backend-go/functions/function"
 )
 
 func main() {
+
+	/*
 	function.Display(10)
 
 	v:= 	function.Add(10, 5)
@@ -12,4 +16,23 @@ func main() {
 	function.Display(v)
 
 	function.RepeatString(5, "Hello World")
-}
+
+	value, err := function.Calc(function.TEST, 10.50, 40)
+	
+	fmt.Println("Result: ", value, " - Error: ", err)
+
+	x, y := function.Split(100)
+	fmt.Println("X: ", x, " - Y: ", y)
+
+	fmt.Println("La suma de sus numeros es: ",function.MSum(10, 20, 30, 40, 50, 1))
+*/
+
+
+//fmt.Println(function.MOperation(function.SUM))
+
+
+factOpFunc := function.FactoryOperation(function.DIV)
+
+fmt.Println(factOpFunc(10, 0))
+
+	}
