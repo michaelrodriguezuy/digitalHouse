fa0c423fda850cddb62104ba2113afd8b141b583
michaelrodriguezuy <hola@olimarteam.uy>
2025-04-10 16:39:21 -0300

    channels en go

--- especialidad backend/go/goroutines/main.go
+++ especialidad backend/go/goroutines/main.go
@@ -17,6 +17,32 @@ func main() {
 
 	time.Sleep(3 * time.Second) //esto es para que el main no termine antes de que terminen los goroutines, cuando este tiempo termine el main se cierra
 	//si no le pongo el sleep el main se cierra antes de que terminen los goroutines
+
+	myFirstChannel := make(chan string)
+
+	go myProceessWithChannel("C", myFirstChannel) //al ser un goroutine no se espera a que termine los procesos anteriores, sino que se ejecuta en paralelo
+
+	//de esta forma se ejecuta el proceso C y se le pasa un canal para que me retorne el resultado, este se queda esperando a que se le asigne un valor al canal, como pasa en la linea 47
+	result := <-myFirstChannel
+	fmt.Println("Result from channel 'myFirstChannel': ", result)
+	
+	close(myFirstChannel)	
+
+
+	channelA := make(chan string)
+	channelB := make(chan string)
+
+	go myProceessWithChannel("D", channelA)
+	go myOtherProceessWithChannel("E", channelB)
+
+	resultA := <-channelA
+	fmt.Println("Result from channel 'channel A': ", resultA)
+
+	resultB := <-channelB
+	fmt.Println("Result from channel 'channel B': ", resultB)
+
+	close(channelA)
+	close(channelB)
 }
 
 func myProcess(p string) {
@@ -26,4 +52,27 @@ func myProcess(p string) {
 		time.Sleep(150 * time.Millisecond) // le forzo un delay
 		fmt.Printf("Processing: %s - num: %d\n", p, i)
 	}
+}
+
+func myProceessWithChannel(p string, c chan string) {
+	// Simulate some processing
+	for i := 0; i < 20; i++ {
+		i++
+		time.Sleep(150 * time.Millisecond) // le forzo un delay
+		fmt.Printf("Processing channel 1: %s - num: %d\n", p, i)
+	}
+	c <- "ok!"
+	
+
+}
+func myOtherProceessWithChannel(p string, c chan string) {
+	// Simulate some processing
+	for i := 0; i < 5; i++ {
+		i++
+		time.Sleep(150 * time.Millisecond) // le forzo un delay
+		fmt.Printf("Processing channel 2: %s - num: %d\n", p, i)
+	}
+	c <- "ok!"
+	
+
 }
